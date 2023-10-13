97a20fc7452893cd1e8fc3a8fb1c62e5f9fd9218
UzomaAkpala <francisakpala@yahoo.com>
2023-10-13 12:22:12 +0100

    Uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rpdktryhtosxyajndsoh.supabase.co";
+export const supabaseUrl = "https://rpdktryhtosxyajndsoh.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwZGt0cnlodG9zeHlham5kc29oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NTIzMTUsImV4cCI6MjAxMjUyODMxNX0.z38nEBjerP5PksaI-eWZM9f3c-kJJK1FW89fIU0SKpY";
 const supabase = createClient(supabaseUrl, supabaseKey);
