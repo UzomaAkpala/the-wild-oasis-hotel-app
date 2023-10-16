0155f3b9d85b59bca9ec7e17e0c999dc093c0b19
UzomaAkpala <francisakpala@yahoo.com>
2023-10-16 14:43:15 +0100

    Editing a Cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,29 +4,36 @@ export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
   return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     ""
   );
 
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
-  //https://rpdktryhtosxyajndsoh.supabase.co/storage/v1/object/public/cabin-images/cabin-005.jpg
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
-  //1. Create cabin
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }]);
+  //1. Create/edit cabin
+  let query = supabase.from("cabins");
+
+  //A) CREATE
+
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  //B) EDIT
+  if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+
+  const { data, error } = await query.select().single();
 
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be created");
   }
 
@@ -39,7 +46,7 @@ export async function createCabin(newCabin) {
 
   if (storageError) {
     await supabase.from("cabins").delete().eq("id", data.id);
-    console.error(storageError);
+
     throw new Error(
       "Cabins image could not be uploaded and the cabin could not be created"
     );
@@ -52,7 +59,6 @@ export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
-    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
